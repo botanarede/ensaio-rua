dd327ff81a4461691c1f9cfa3da9adbd7584c6ca
Bota na Rede <contato@botanarede.com.br>
2025-05-14 12:22:22 +0000

    _next

---
+++ _next/static/chunks/pages/_app-fab15dc2ab2e6536.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{8490:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(3888)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(8490),n(9646))),_N_E=_.O()}]);
